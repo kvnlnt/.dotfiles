8de29b56d2adb03d1933308bf56d901fd856e177
Kevin Lint <kevin@coder.com>
2021-01-06 09:45:47 -0800

    voila

---
+++ bash/.bash_aliases
@@ -0,0 +1,8 @@
+alias ll="ls -la"
+alias ld='ls -la --group-directories-first'
+alias lp='sed "s/:/\n/g" <<< "$PATH"'
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias chrome='google-chrome'
+alias docker-clean='docker system prune -a'
--- install.sh
+++ install.sh
@@ -1,1 +1,24 @@
-cp -r .alias .bashrc .exports .alias .functions .prompt .gitconfig .vimrc .inputrc ~
+#!/usr/bin/env bash
+
+echo "Running: install.sh"
+echo "Cloning: HOME dotfiles..."
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Install HOME-based bash files
+ln -s $DIR/bash/.bash_aliases $HOME
+ln -s $DIR/bash/.bash_path $HOME
+ln -s $DIR/bash/.bashrc $HOME
+ln -s $DIR/bash/.exports $HOME
+ln -s $DIR/bash/.functions $HOME
+ln -s $DIR/bash/.inputrc $HOME
+ln -s $DIR/bash/.profile $HOME
+ln -s $DIR/bash/.prompt $HOME
+ln -s $DIR/bash/.vimrc $HOME
+
+# Install git files
+ln -s $DIR/git/.gitconfig $HOME
+
+# Install VS Code files
+mkdir -p $HOME/.config/Code/User
+ln -s $DIR/vscode/keybindings.json $HOME/.config/Code/User
+ln -s $DIR/vscode/settings.json $HOME/.config/Code/User
